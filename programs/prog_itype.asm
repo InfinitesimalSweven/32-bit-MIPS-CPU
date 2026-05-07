9cbc298c5144ab2782dd07c59acfbdccfa561e62
Tanzeem Hasan <tanzeem.hasan@cooper.edu>
2026-05-07 16:28:45 -0400

    added some programs to get timing diagrams

---
+++ programs/prog_itype.asm
@@ -0,0 +1,4 @@
+addi $t0, $zero, 5
+sw   $t0, 84($zero)
+lw   $t1, 84($zero)
+sw   $zero, 252($zero)s
---
+++ programs/prog_rtype.asm
@@ -0,0 +1,5 @@
+addi $t0, $zero, 5
+addi $t1, $zero, 3
+add  $t2, $t0, $t1
+sw   $t2, 84($zero)
+sw   $zero, 252($zero)
